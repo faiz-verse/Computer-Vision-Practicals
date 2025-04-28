abedeaff45ffdb30ead5e40dca07243f8fadecf6
faiz-verse <faizzzverseee@gmail.com>
2025-04-28 10:53:14 +0530

    practiced 1D transformation

--- Practice/1DTransformation.py
+++ Practice/1DTransformation.py
@@ -4,30 +4,28 @@ import matplotlib.pyplot as plt
 x = np.array([0,1,2,3,4,5])
 y = np.array([2,3,5,7,11,13])
 
-plt.plot(x,y, label='original points', marker='o')
-plt.title('Initial 1D Points')
+plt.plot(x,y, label='Original', marker='o')
+plt.title('1D Points')
 plt.legend()
 plt.grid()
 plt.show()
 
-# Translation
-xt = x + 2
-plt.plot(x,y, label='original points', marker='o')
-plt.plot(xt,y, label='translated points', marker='x')
-plt.title('1D Translation')
+x_trans = x + 2
+y_trans = y + 1
+plt.plot(x,y, label='Original', marker='o')
+plt.plot(x_trans,y_trans, label='Translated', marker='o')
+plt.title('Translated Points (x by 2, y by 1)')
 plt.legend()
 plt.grid()
 plt.show()
 
-# Scaling
-xs = x * 2
-ys = y * 0.5
-plt.plot(x,y, label='original points', marker='o')
-plt.plot(xs,y, label='x scaled points', marker='x')
-plt.plot(x,ys, label='y scaled points', marker='x')
-plt.title('1D Scaling')
+x_scaled = x * 2
+y_scaled = y * 0.5
+plt.plot(x,y, label='Original', marker='o')
+plt.plot(x_scaled,y, label='X Scaled', marker='o')
+plt.plot(x,y_scaled, label='Y Scaled', marker='x')
+plt.title('Scaled Points (x by 2, y by 0.5)')
 plt.legend()
 plt.grid()
 plt.show()
-
 
